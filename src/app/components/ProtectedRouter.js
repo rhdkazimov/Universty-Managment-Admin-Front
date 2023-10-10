cd3f6637542adfe66b71072d4eb392a419ef0883
kazimov789 <118839541+kazimov789@users.noreply.github.com>
2023-10-10 11:02:47 +0400

    Handle Router AdminLogin Problem

--- src/app/components/ProtectedRouter.js
+++ src/app/components/ProtectedRouter.js
@@ -1,11 +1,18 @@
-import React from 'react'
-import { Navigate } from 'react-router-dom'
-import { ROUTES } from '../../Routes/consts'
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { ROUTES } from "../../Routes/consts";
 
-
-export const ProtectedRouter  = ({children}) => {
-  if(localStorage.getItem("token")){
-   return children 
+export const ProtectedRouter = ({ children }) => {
+  if (localStorage.getItem("token")) {
+    const user = localStorage.getItem("user");
+    if (user.isAdmin) return children;
+    else {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      return <Navigate to={ROUTES.ADMIN.LOGIN} />;
+    }
   }
-  return <Navigate to={ROUTES.ADMIN.LOGIN}/>
-}
+  localStorage.removeItem("token");
+  localStorage.removeItem("user");
+  return <Navigate to={ROUTES.ADMIN.LOGIN} />;
+};
